b06ab31892b8f162bfc665a8a8200b256082bbda
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-06-19 13:43:23 +0530

    initial commit

---
+++ block-BJaajw/code/index.js
@@ -0,0 +1,49 @@
+const source = document.querySelector('#source');
+const root = document.querySelector('.root');
+let allnews;
+
+source.addEventListener('change', (event) => {
+   if(event.target.value === 'default'){
+    createUI(allnews);
+   }else{
+    let filteredNews = allnews.filter(ele => {
+        return ele.newsSite.toLowerCase().split(" ").join("") === event.target.value;
+    })
+    createUI(filteredNews);
+   }
+});
+
+function createUI(news) {
+    root.innerHTML =""
+    news.forEach(eachNews => {
+        let innerHtml = `
+          <div class="img-container flex-40">
+              <img class="full-width" src="${eachNews.imageUrl}" alt="">
+          </div>
+          <div class="flex-56">
+              <h3>${eachNews.newsSite}</h3>
+              <p>${eachNews.title}</p>
+              <a href="${eachNews.url}" target='_blank'>Read More</a>
+          </div>`
+        let article = document.createElement('article');
+        article.classList.add('flex','relative');
+        article.innerHTML = innerHtml;  
+        root.append(article);
+    })
+}
+
+fetch('https://api.spaceflightnewsapi.net/v3/articles?_limit=30')
+  .then((res) => {
+      if(!res.ok){
+          throw new Error(`network Error: ${res.status}`)
+      }
+      return res.json()
+  })
+  .then((data)=>{
+      if(Array.isArray(data)){
+        allnews = data;
+        createUI(data);
+      }
+  }).catch(error => {
+      root.innerText = error;
+  })
