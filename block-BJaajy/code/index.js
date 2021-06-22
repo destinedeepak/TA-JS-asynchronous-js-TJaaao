d54247c6d6b1e50aef0a53b55f06384081b9feec
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-06-22 15:34:02 +0530

    refactore code

--- block-BJaajy/code/index.js
+++ block-BJaajy/code/index.js
@@ -1,20 +1,17 @@
 const p1 = new Promise((res, rej) => {
-    setTimeout(res("one"),1000)
-})
+  setTimeout(res('one'), 1000);
+});
 const p2 = new Promise((res, rej) => {
-    setTimeout(res("two"),2000)
-})
+  setTimeout(res('two'), 2000);
+});
 const p3 = new Promise((res, rej) => {
-    setTimeout(res("three"),3000)
-})
+  setTimeout(res('three'), 3000);
+});
 const p4 = new Promise((res, rej) => {
-    setTimeout(res("four"),4000)
-})
+  setTimeout(res('four'), 4000);
+});
 
-Promise.all([p1,p2,p3,p4]).then(res =>console.log(res));
-
-
-
+Promise.all([p1, p2, p3, p4]).then((res) => console.log(res));
 
 // const userNames = ['destinedeepak'];
 
@@ -27,16 +24,9 @@ Promise.all([p1,p2,p3,p4]).then(res =>console.log(res));
 //     users.forEach(user => console.log(user.followers))
 // })
 
+let urlList = [`https://random.dog/woof.json`, `https://aws.random.cat/meow`];
 
-
-let urlList = [`https://random.dog/woof.json`,`https://aws.random.cat/meow`];
-
- Promise.race(urlList.map(url => fetch(url)))
- .then(console.log)
-
-
-
-
+Promise.race(urlList.map((url) => fetch(url))).then(console.log);
 
 const one = new Promise((resolve, reject) =>
   setTimeout(() => resolve('Arya'), 1000)
@@ -48,23 +38,21 @@ const three = new Promise((resolve, reject) =>
   setTimeout(() => resolve('John'), 3000)
 );
 
-Promise.allSettled([one, two, three])
-.then(data => console.log(data,' allSettled'))
+Promise.allSettled([one, two, three]).then((data) =>
+  console.log(data, ' allSettled')
+);
 
 let allPromises = Promise.all([one, two, three]);
 // Not working with all becuse one of the promise is not fullfilled
 
-
-
-
 Promise.all([
-    new Promise((resolve, reject) => {
-      setTimeout(() => resolve('Arya'), 1000);
-    }),
-    'Sam',
-    { name: 'John' },
-  ])
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Arya'), 1000);
+  }),
+  'Sam',
+  { name: 'John' },
+])
   .then(console.log)
-  .catch(console.log)
-  //output is a promise array with all the resolved values, 1sec
+  .catch(console.log);
+//output is a promise array with all the resolved values, 1sec
 
